ccff23115e82f2e8e3ce06a2009454747df25cec
Softjit Singh <besofttech8@gmail.com>
2025-04-06 21:14:59 +0530

    cart number

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -2,15 +2,37 @@ import Link from 'next/link'
 import Image from 'next/image'
 const Productslist = () => {
   return (
-    // <div className='text-center font-extrabold text-8xl'>
-    //   huhhh! Eve Khrid Loge Mitra nu
-    // </div>
-
-    <div className='flex gap-x-8 gap-y-16 justify-between flex-wrap'>
-      <Link href='/products' className='relative w-full h-80'>
-        <Image src='/models/m2.jpg' fill sizes='25vw' />
-      </Link>
-    </div>
+    <>
+      <div className='mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap'>
+        <Link
+          href='/products'
+          className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'
+        >
+          <div className='relative h-80 w-full'>
+            <Image
+              src='/models/m2.jpg'
+              fill
+              sizes='25vw'
+              className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease-in-out duration-500'
+            />
+            <Image
+              src='/models/m1.jpg'
+              fill
+              sizes='25vw'
+              className='absolute object-cover rounded-md'
+            />
+          </div>
+          <div className='flex justify-between'>
+            <span className='font-medium'>Product Name</span>
+            <span className='font-semibold'>RS. 85</span>
+          </div>
+          <div className='text-sm text-gray-500'>Description</div>
+          <button className='rounded-2xl ring-1 ring-sg text-sg py-2 px4 hover:bg-sg hover:text-white'>
+            Add to Cart
+          </button>
+        </Link>
+      </div>
+    </>
   )
 }
 export default Productslist
--- app/products/page.jsx
+++ app/products/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 const page = () => {
   return (
-    <div className='text-center font-extrabold text-8xl p-44'>
+    <div className='text-center font-extrabold text-6xl lg:text-8xl xl:p-44'>
       huhhh! Eve Khrid Loge Mitra nu
     </div>
   )
