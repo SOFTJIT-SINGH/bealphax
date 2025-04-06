816bfd911d8b5f196bf44ae5e0727cda66c93ddc
Softjit Singh <besofttech8@gmail.com>
2025-04-06 22:01:55 +0530

    cart number

---
+++ app/components/Catlist.jsx
@@ -0,0 +1,58 @@
+'use client'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Catlist = () => {
+  const categories = [
+    { name: 'Gucci', image: '/models/m2.jpg' },
+    { name: 'Prada', image: '/models/m1.jpg' },
+    { name: 'LV', image: '/models/m3.jpg' },
+    { name: 'Balenciaga', image: '/models/m4.jpg' },
+    { name: 'Armani', image: '/models/m5.jpg' },
+    { name: 'Versace', image: '/models/m3.jpg' },
+    { name: 'Zara', image: '/models/m2.jpg' },
+  ]
+
+  return (
+    <div className='px-4 py-6'>
+      <h2 className='text-2xl font-semibold mb-4'>Shop by Brand</h2>
+
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={2}
+        loop={true}
+        autoplay={{ delay: 3000 }}
+        modules={[Navigation, Autoplay]}
+        breakpoints={{
+          640: { slidesPerView: 3 },
+          768: { slidesPerView: 4 },
+          1024: { slidesPerView: 5 },
+        }}
+      >
+        {categories.map((cat, index) => (
+          <SwiperSlide key={index}>
+            <Link href={`/products?cat=${cat.name.toLowerCase()}`}>
+              <div className='relative w-full h-64 bg-slate-100 rounded-lg overflow-hidden'>
+                <Image
+                  src={cat.image}
+                  fill
+                  className='object-cover'
+                  alt={cat.name}
+                />
+              </div>
+              <h1 className='mt-4 font-light text-lg tracking-wide text-center'>
+                {cat.name}
+              </h1>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
+}
+
+export default Catlist
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@
 // import Link from 'next/link'
 import Slider from './components/Slider2'
 import Productslist from './components/Products'
+import Catlist from './components/Catlist'
 export default function Home() {
   return (
     <>
       <Slider />
+      <div className='mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+        <h1 className='text-2xl'> Feature Products</h1>
+        <Productslist />
+      </div>
+      <div className='mt-24'>
+        <h1 className='text-2xl'> Categories</h1>
+        <Catlist />
+      </div>
       <div className='mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
         <h1 className='text-2xl'> Feature Products</h1>
         <Productslist />
