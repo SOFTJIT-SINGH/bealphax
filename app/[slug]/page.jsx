cf3c244ab77f125b5b421ba53c54a0b0af992634
Softjit Singh <besofttech8@gmail.com>
2025-04-07 23:00:14 +0530

    cart number

---
+++ app/[slug]/page.jsx
@@ -0,0 +1,15 @@
+import Productimg from '../components/Productimg'
+
+const Singlepage = () => {
+  return (
+    <div className='mt-2 h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
+      {/* img  */}
+      <div className='w-full lg:w-1/2 lg:sticky top-20 h-max'>
+        <Productimg />
+      </div>
+      {/* text   */}
+      <div className='w-full lg:w-1/2 flex flex-col gap-6'> texts</div>
+    </div>
+  )
+}
+export default Singlepage
---
+++ app/components/Productimg.jsx
@@ -0,0 +1,8 @@
+const Productimg = () => {
+  return (
+    <div>
+      <div className='h-[500px] relative'></div>
+    </div>
+  )
+}
+export default Productimg
